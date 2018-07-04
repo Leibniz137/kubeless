dea68d5adb96e7a7bb22fbc4fe590e1fe007e987
Leibniz137 <ncg09@hampshire.edu>
2018-07-04 10:45:51 -0700

    add ability to set FUNC_PORT at runtime

--- docker/runtime/rust/test_runtime.py
+++ docker/runtime/rust/test_runtime.py
@@ -13,24 +13,36 @@ def image():
     yield name
 
 
-@pytest.fixture(scope="session")
-def container(image):
-    container_id = subprocess.check_output(
-        shlex.split('docker run -d -p 8000:8000 {}'.format(image))
-    ).rstrip(b'\n').decode('utf-8')
-    yield container_id
-    subprocess.check_call(shlex.split('docker rm -f {}'.format(container_id)))
+class TestContainer:
+    @pytest.fixture(scope="class")
+    def container(self, image):
+        container_id = subprocess.check_output(
+            shlex.split('docker run -d -p 8000:8000 {}'.format(image))
+        ).rstrip(b'\n').decode('utf-8')
+        yield container_id
+        subprocess.check_call(shlex.split('docker rm -f {}'.format(container_id)))
+
+    def test_healthz(self, container):
+        resp = requests.get('http://localhost:8000/healthz')
+        resp.raise_for_status()
+        assert resp.text == 'OK'
 
 
-def test_8080(container):
-    resp = requests.get('http://localhost:8000')
-    assert resp.ok
+class TestFUNCPORT:
+    @pytest.fixture(scope="class")
+    def container(self, image):
+        funcport = '4242'
+        cmd = shlex.split('docker run -d -e "FUNC_PORT={funcport}" -p 8000:{funcport} {image}'
+            .format(funcport=funcport, image=image)
+        )
+        container_id = subprocess.check_output(cmd).rstrip(b'\n').decode('utf-8')
+        yield container_id
+        subprocess.check_call(shlex.split('docker rm -f {}'.format(container_id)))
 
-
-def test_healthz(container):
-    resp = requests.get('http://localhost:8000/healthz')
-    resp.raise_for_status()
-    assert resp.text == 'OK'
+    def test_healthz(self, container):
+        resp = requests.get('http://localhost:8000/healthz')
+        resp.raise_for_status()
+        assert resp.text == 'OK'
 
 
 # def test_failing_function():
